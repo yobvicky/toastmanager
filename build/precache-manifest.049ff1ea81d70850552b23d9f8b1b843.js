730728a5d80eee9cb64d2f314a26a949a8d0cddf
Sivakumar, Vignesh <VIGNESH_SIVAKUMAR@homedepot.com>
2020-05-25 21:02:53 -0400

    adding build folder

---
+++ build/precache-manifest.049ff1ea81d70850552b23d9f8b1b843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f70e45706fc8715bb465f0206f41f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221d4c2d16c539ba14fb",
+    "url": "/static/css/main.2b72ecdf.chunk.css"
+  },
+  {
+    "revision": "be0774fc6171f365b3b4",
+    "url": "/static/js/2.586f208d.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/static/js/2.586f208d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221d4c2d16c539ba14fb",
+    "url": "/static/js/main.9097e05d.chunk.js"
+  },
+  {
+    "revision": "967c3795ab38809f7b6c",
+    "url": "/static/js/runtime-main.f176ae03.js"
+  }
+]);
